58e29074fe14b4f68102a610a1fd86f925a0968d
Taehee Ryu <ice1250@gmail.com>
2023-02-10 15:19:22 +0900

    기본구조

---
+++ lib/screens/edit_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EditScreen extends StatefulWidget {
+  const EditScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditScreen> createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Edit',
+          style: TextStyle(
+            fontSize: 24,
+          )),
+    );
+  }
+}
