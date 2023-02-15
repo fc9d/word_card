b9f29101431cd830cf0d99aaa455037974cf0060
kkuld21 <kkuld21@gmail.com>
2023-02-15 12:44:01 +0900

    일단 수정하기 리스트 와꾸잡고 사무실가서 작업하기

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -8,13 +8,71 @@ class EditScreen extends StatefulWidget {
 }
 
 class _EditScreenState extends State<EditScreen> {
+  List testList = [];
+  String input = '';
+
+  void initState() {
+    super.initState();
+    testList.add("Item1");
+    testList.add("Item2");
+    testList.add("Item3");
+    testList.add("Item4");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Edit',
-          style: TextStyle(
-            fontSize: 24,
-          )),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                    title: Text("Add Todolist"),
+                    content: TextField(
+                      onChanged: (String value) {
+                        input = value;
+                      },
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                          onPressed: () {
+                            setState(() {
+                              testList.add(input);
+                            });
+                            Navigator.of(context).pop(); // input 입력 후 창 닫히도록
+                          },
+                          child: Text("Add"))
+                    ]);
+              });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: testList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+                // 삭제 버튼 및 기능 추가
+                key: Key(testList[index]),
+                child: Card(
+                    elevation: 4,
+                    margin: EdgeInsets.all(8),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    child: ListTile(
+                      title: Text(testList[index]),
+                      trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            setState(() {
+                              testList.removeAt(index);
+                            });
+                          }),
+                    )));
+          }),
     );
   }
 }
